820ef0358ab435d0c8653ec871e8475e4c6bd42b
genessiescobar <genessialex@gmail.com>
2020-03-25 22:26:52 -0430

    registro

---
+++ Formulario/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+<strong>Bienvenidos</strong>
+<ul>
+	<li><a href="formulario.html"></a>Formulario</li>
+	<li><a href="#"></a>Modulo 2</li>
+	<li><a href="#"></a>Modulo 3</li>
+</ul>
+</body>
+</html>
